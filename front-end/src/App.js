654a50523ebb204e36222ddcb670a4d6867c398f
0303vikas <2751vikassing@gmail.com>
2022-11-04 01:18:40 +0200

    adding front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import NavBar from './views/navigationbar'
+import Cards from './views/displayCard'
+
+function App() {
+    return (
+        <>
+            <NavBar />
+            <Cards />
+
+        </>
+    )
+}
+
+export default App
---
+++ front-end/src/views/displayCard.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import '../App.css'
+
+const Cards = () => {
+
+    return(
+        <div className="mainDiv">
+            <div className='bubble'>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className='group'>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card">
+                    
+                    {/* <div className='div'>kkj</div>
+                    <div className='div'>kjnm</div>
+                    <div className='div'></div>
+                    <div className='div'></div> */}
+                </div>
+                
+                <div className="small-card card"></div>
+            </div>
+            <div className='group2 group'>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card">
+                    
+                    {/* <div className='div'>kkj</div>
+                    <div className='div'>kjnm</div>
+                    <div className='div'></div>
+                    <div className='div'></div> */}
+                </div>
+                
+                <div className="small-card card"></div>
+            </div>
+            <div className='group3 group'>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card">
+                    
+                    {/* <div className='div'>kkj</div>
+                    <div className='div'>kjnm</div>
+                    <div className='div'></div>
+                    <div className='div'></div> */}
+                </div>
+                
+                <div className="small-card card"></div>
+            </div>
+            <div className='group3 group'>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"></div>
+                <div className="large-card card">
+                    
+                    {/* <div className='div'>kkj</div>
+                    <div className='div'>kjnm</div>
+                    <div className='div'></div>
+                    <div className='div'></div> */}
+                </div>
+                
+                <div className="small-card card"></div>
+            </div>
+            
+        </div>
+
+    )
+}
+
+export default Cards
