66a0f6246788793b22280f413742ba6b49027cda
0303vikas <2751vikassing@gmail.com>
2022-11-05 14:06:23 +0200

    changing the ui of the cards

--- front-end/src/views/displayCard.js
+++ front-end/src/views/displayCard.js
@@ -1,5 +1,15 @@
 import React from 'react'
-import '../App.css'
+import './displayCard.css'
+
+const LargeDiv2 = () => {
+    return(
+        <div style={{margin: '4px',width: '70%', height: '100%', backgroundColor: 'rgb(200, 223, 82)'}}>
+            <img src='https://cdn.pixabay.com/photo/2019/10/24/19/50/sloth-4575121__480.png' alt='tripImage' style={{width: '30%', height: '30%'}} />
+            
+
+        </div>
+    )
+}
 
 const Cards = () => {
 
@@ -7,72 +17,95 @@ const Cards = () => {
         <div className="mainDiv">
             
             <div className='group'>
-                <div className="large-card card"></div>
-                <div className="small-card card"></div>
-                <div className="large-card card"></div>
-                <div className="small-card card"></div>
-                <div className="large-card card"></div>
-                <div className="small-card card"></div>
                 <div className="large-card card">
-                    
-                    {/* <div className='div'>kkj</div>
-                    <div className='div'>kjnm</div>
-                    <div className='div'></div>
-                    <div className='div'></div> */}
+
+                </div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
+                <div className="large-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
+                <div className="large-card card">
+                    <LargeDiv2 />
+                </div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
+                <div className="large-card card">
+                    <div>
+                        <div className='div'></div>
+                        <div className='div'></div>
+                    </div>
+                    {/* <div>
+                        <div className='div'>jdsjj</div>
+                        <div className='div'>djdk</div>
+                    </div> */}
                 </div>
                 
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
             </div>
             <div className='group2 group'>
                 <div className="large-card card"></div>
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
                 <div className="large-card card"></div>
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
                 <div className="large-card card"></div>
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
                 <div className="large-card card">
-                    
-                    {/* <div className='div'>kkj</div>
-                    <div className='div'>kjnm</div>
-                    <div className='div'></div>
-                    <div className='div'></div> */}
+                    <div>
+                        <div className='div'></div>
+                        <div className='div'></div>
+                    </div>
+                    {/* <div>
+                        <div className='div'>jdsjj</div>
+                        <div className='div'>djdk</div>
+                    </div> */}
                 </div>
                 
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
             </div>
             <div className='group3 group'>
                 <div className="large-card card"></div>
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
                 <div className="large-card card"></div>
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
                 <div className="large-card card"></div>
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
                 <div className="large-card card">
-                    
-                    {/* <div className='div'>kkj</div>
-                    <div className='div'>kjnm</div>
-                    <div className='div'></div>
-                    <div className='div'></div> */}
+                    <div>
+                        <div className='div'></div>
+                        <div className='div'></div>
+                    </div>
+                    {/* <div>
+                        <div className='div'>jdsjj</div>
+                        <div className='div'>djdk</div>
+                    </div> */}
                 </div>
                 
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
             </div>
             <div className='group3 group'>
                 <div className="large-card card"></div>
-                <div className="small-card card"></div>
-                <div className="large-card card"></div>
-                <div className="small-card card"></div>
-                <div className="large-card card"></div>
-                <div className="small-card card"></div>
-                <div className="large-card card">
+                <div className="small-card card">
+                    <img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} />
                     
-                    {/* <div className='div'>kkj</div>
-                    <div className='div'>kjnm</div>
-                    <div className='div'></div>
-                    <div className='div'></div> */}
+                </div>
+                <div className="large-card card"></div>
+                <div className="small-card card">
+                    <img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} />
+                </div>
+                <div className="large-card card"></div>
+                <div className="small-card card">
+                    <img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} />
+                </div>
+                <div className="large-card card">
+                    <div>
+                        <div className='div'></div>
+                        <div className='div'></div>
+                    </div>
+                    {/* <div>
+                        <div className='div'>jdsjj</div>
+                        <div className='div'>djdk</div>
+                    </div> */}
                 </div>
                 
-                <div className="small-card card"></div>
+                <div className="small-card card"><img src='https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600' alt='tripImage' style={{width: 'inherit', height: '100%', borderRadius: 'inherit'}} /></div>
             </div>
             
         </div>
