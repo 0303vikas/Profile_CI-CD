d0bdc5b3a63e71902b99d675729ee8aa6ed3fe5e
0303vikas <vikas.singh@edu.turkuamk.fi>
2022-11-23 01:07:03 +0200

    adding test cases and setting up env

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+// import App from '../src/views/App'
+import NavBar from './views/navigationbar'
+import LoginForm from './views/Login'
+import {BrowserRouter as Router} from 'react-router-dom';
+import React from 'react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testing Front-end', () => {
+
+  describe('Navigation Bar test', () => {
+  var container
+    beforeEach(() => {
+      ({container} = render(<Router><NavBar /></Router>))
+    })
+
+    
+
+    test('navbar exists', () => {     
+      const linkElement = container.getElementsByClassName('navigation')
+      expect(linkElement).toBeDefined()
+     })
+ 
+     test('elements in Navigation bar', () => {
+       const button1 = screen.getByText('Add')
+       const button2 = screen.getByText('MyCollection')
+       const button3 = screen.getByText('Login')
+ 
+       expect(button1).toBeInTheDocument()
+       expect(button2).toBeInTheDocument()
+       expect(button3).toBeInTheDocument()
+     })
+
+     
+
+
+
+  })
+
+  
+
+
+}
+
+)
+
+
+
