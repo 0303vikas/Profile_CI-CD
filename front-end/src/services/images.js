56aeb39e59b50c61f32f8a9df9ff7cddcf9be785
0303vikas <2751vikassing@gmail.com>
2022-11-24 10:58:56 +0200

    changing localhost url

--- front-end/src/services/images.js
+++ front-end/src/services/images.js
@@ -7,10 +7,10 @@ const getAll = async () => {
 }
 
 const postImages = async (userdata,imageArray) => {
-    const request = await axios.post('api/images',imageArray, {
+    const request = await axios.post('/api/images',imageArray, {
         headers: {
             'content-type': 'multipart/form-data',
-            'Authorization': 'Bearer '+userdata.token
+            'Authorization': 'Bearer '+ userdata.token
 
         },
         onUploadProgress: progressEvent => console.log(Math.round( (progressEvent.loaded * 100) / progressEvent.total ))
