1930121e314a4926306f4317e5fe76047dcf23e4
0303vikas <vikas.singh@edu.turkuamk.fi>
2022-11-27 18:29:38 +0200

    fixing adding image error

--- front-end/src/views/Login.js
+++ front-end/src/views/Login.js
@@ -29,7 +29,7 @@ const LoginForm = ({userUpdate}) => {
             )
 
             userUpdate(user)
-            setTimeout(() => navigate('/add'),3000)
+            navigate('/add')
 
             
             
