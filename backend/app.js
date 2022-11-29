84e41364da263c60dd640cbad2433c535807e54a
0303vikas <2751vikassing@gmail.com>
2022-11-29 11:17:18 +0200

    changes in the backend

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'test') {
 
 
 app.use(cors())
-// app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}))
 
